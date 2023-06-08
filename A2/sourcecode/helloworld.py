e23d8a645c5d5a582da64f8a2074da78333ee989
Kovarthanan-murugan <kovarthan.ece@gmail.com>
2023-06-08 19:25:40 -0300

    Add source code

---
+++ A2/sourcecode/helloworld.py
@@ -0,0 +1,62 @@
+import grpc
+import helloworld_pb2
+import helloworld_pb2_grpc
+import boto3
+from concurrent import futures
+from urllib.parse import urlparse
+
+class EC2OperationsServicer(helloworld_pb2_grpc.EC2OperationsServicer):
+    bucketname = "b00936880learning" 
+    key = "test.txt"
+
+    def StoreData(self, request, context):
+        # TODO: Implement the logic to store data
+
+        s3 = boto3.resource('s3')
+        data = request.data
+        url = "https://"+self.bucketname+".s3.amazonaws.com/"+self.key
+        s3.Bucket(self.bucketname).put_object(Key=self.key, Body=data)
+        # print(url1)
+        response = helloworld_pb2.StoreReply(s3uri='Data stored successfully')
+        return response
+
+    def AppendData(self, request, context):
+        # TODO: Implement the logic to append data
+        s3 = boto3.client('s3')
+        # Retrieve the existing content of the file
+        response = s3.get_object(Bucket=self.bucketname, Key=self.key)
+        existing_content = response['Body'].read().decode('utf-8')
+
+        # Append the new content to the existing content
+        updated_content = existing_content + request.data
+        print(updated_content)
+        s3.put_object(Body=updated_content, Bucket=self.bucketname, Key=self.key)
+        response = helloworld_pb2.AppendReply()
+        return response
+
+    def DeleteFile(self, request, context):
+        # TODO: Implement the logic to delete a file
+        
+        parsed_url = urlparse(request.s3uri)
+        bucket_name = parsed_url.netloc.split('.')[0]
+        object_key = parsed_url.path.lstrip('/')
+        print(parsed_url)
+        print(bucket_name)
+        print(object_key)
+        s3 = boto3.client('s3')
+        s3.delete_object(Bucket=bucket_name,Key=object_key)
+        response = helloworld_pb2.DeleteReply()
+        return response
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    helloworld_pb2_grpc.add_EC2OperationsServicer_to_server(EC2OperationsServicer(), server)
+    server.add_insecure_port('[::]:50052')
+    server.start()
+    print("Server started, listening on port 50051")
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
